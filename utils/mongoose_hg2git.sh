c0f5a6581d5cc2dc046a4fd22f7eabc388e50827
Ger Hobbelt <ger@hobbelt.com>
2011-05-08 17:28:33 +0200

    augmented the mongoose HG to GIT export script to allow it being run multiple times while ensuring all config files are properly patched during the (initial) run.

--- utils/mongoose_hg2git.sh
+++ utils/mongoose_hg2git.sh
@@ -14,20 +14,97 @@ easy_install hg-git
 hg clone https://mongoose.googlecode.com/hg/ mongoose
 cd mongoose
 
+# copy the config file to edit to tempfile; create an empty file to process when the config file does not exist yet.
+# Done this way so gawk can write the result to the config file location.
+tmpscript=`mktemp`
+cat > ${tmpscript}.awk  <<EOT
+#
 # edit ~/.hgrc and add :
 #
 # [extensions]
 # hgext.bookmarks =
 # hggit =
 #
+BEGIN {
+	has_ext = 0;
+}
+
+/extensions/	{
+	has_ext++;
+}
+
+/hggit/		{
+	has_ext++;
+}
+
+END {
+	switch (has_ext)
+	{
+	case 0:
+	case 1:
+		printf("\n[extensions]\n");
+		printf("hgext.bookmarks =\n");
+		printf("hggit =\n");
+		break;
+
+	default:
+		break;
+	}
+}
+
+	{
+	printf("%s\n", \$0);
+}
+
+EOT
+cat ~/.hgrc > ${tmpscript}.cfg
+gawk -f ${tmpscript}.awk ${tmpscript}.cfg > ~/.hgrc
+
 
 hg bookmark -r default master
 
+
+cat > ${tmpscript}.awk  <<EOT
+#
 # edit: .hg/hgrc and add:
 #
 # [git]
 # intree = true
 #
+BEGIN {
+	has_git = 0;
+}
+
+/git/	{
+	has_git++;
+}
+
+/intree/	{
+	if (has_git) has_git++;
+}
+
+END	{
+	switch (has_git)
+	{
+	case 0:
+		printf("\n[git]\n");
+	case 1:
+		printf("intree = true\n");
+		break;
+
+	default:
+		break;
+	}
+}
+
+	{
+	printf("%s\n", \$0);
+}
+
+EOT
+cat .hg/hgrc > ${tmpscript}.cfg
+gawk -f ${tmpscript}.awk ${tmpscript}.cfg > .hg/hgrc
+
 
 hg pull
 hg gexport
