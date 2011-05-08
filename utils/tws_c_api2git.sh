9dda238702c18991ffe3c07166915e7185d52556
Ger Hobbelt <ger@hobbelt.com>
2011-05-08 12:04:01 +0200

    add script to import TWS C API from SF - not as a module but directly.

---
+++ utils/tws_c_api2git.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# pull old TWS C API code from SF; use that as a base for the new work
+#
+
+mkdir ../tws_c_api
+pushd ../tws_c_api
+git cvsimport -k -a -v -d :pserver:anonymous@tws-c-api.cvs.sourceforge.net:/cvsroot/tws-c-api tws_client_api 
+popd
+
