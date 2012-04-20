8a39d6c4bfa0bb6557dc860dafd310ceff4d08f5
Ger Hobbelt <ger@hobbelt.com>
2012-04-20 03:50:19 +0200

    more work on IB/TWS comm chain.

--- tws_request.h
+++ tws_request.h
@@ -79,8 +79,6 @@ protected:
 /* sends message REQ_SCANNER_PARAMETERS to IB/TWS */
 class ib_msg_req_scanner_parameters: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_scanner_parameters(tier2_message_processor *from, tier2_message_processor *to) :
 		tws_request_message(from, to)
@@ -107,8 +105,6 @@ public:
 /* sends message REQ_SCANNER_SUBSCRIPTION to IB/TWS */
 class ib_msg_req_scanner_subscription: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 protected:
 	int ticker_id;
 	ib_scanner_subscription *subscription;
@@ -140,8 +136,6 @@ public:
 /* sends message CANCEL_SCANNER_SUBSCRIPTION to IB/TWS */
 class ib_msg_cancel_scanner_subscription: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 protected:
 	int ticker_id;
 
@@ -172,8 +166,6 @@ public:
 /* sends message REQ_MKT_DATA to IB/TWS */
 class ib_msg_req_mkt_data: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_mkt_data(tier2_message_processor *from, tier2_message_processor *to, int ticker_id, ib_contract *contract, const ib_ticker_list *generic_tick_list, int snapshot) :
 	  tws_request_message(from, to)
@@ -200,8 +192,6 @@ public:
 /* sends message REQ_HISTORICAL_DATA to IB/TWS */
 class ib_msg_req_historical_data: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_historical_data(tier2_message_processor *from, tier2_message_processor *to, int ticker_id, ib_contract *contract, time_t end_date_time, double duration, const char bar_size_setting[], const char what_to_show[], int use_rth, int format_date) :
 	  tws_request_message(from, to)
@@ -228,8 +218,6 @@ public:
 /* sends message CANCEL_HISTORICAL_DATA to IB/TWS */
 class ib_msg_cancel_historical_data: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_cancel_historical_data(tier2_message_processor *from, tier2_message_processor *to, int ticker_id) :
 	  tws_request_message(from, to)
@@ -256,8 +244,6 @@ public:
 /* sends message CANCEL_MKT_DATA to IB/TWS */
 class ib_msg_cancel_mkt_data: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_cancel_mkt_data(tier2_message_processor *from, tier2_message_processor *to, int ticker_id) :
 	  tws_request_message(from, to)
@@ -284,8 +270,6 @@ public:
 /* sends message EXERCISE_OPTIONS to IB/TWS */
 class ib_msg_exercise_options: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_exercise_options(tier2_message_processor *from, tier2_message_processor *to, int ticker_id, ib_contract *contract, int exercise_action, int exercise_quantity, const char account[], int exc_override) :
 	  tws_request_message(from, to)
@@ -312,8 +296,6 @@ public:
 /* sends message PLACE_ORDER to IB/TWS */
 class ib_msg_place_order: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_place_order(tier2_message_processor *from, tier2_message_processor *to, int order_id, ib_contract *contract, ib_order *order) :
 	  tws_request_message(from, to)
@@ -340,8 +322,6 @@ public:
 /* sends message CANCEL_ORDER to IB/TWS */
 class ib_msg_cancel_order: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_cancel_order(tier2_message_processor *from, tier2_message_processor *to, int order_id) :
 	  tws_request_message(from, to)
@@ -368,8 +348,6 @@ public:
 /* sends message REQ_OPEN_ORDERS to IB/TWS */
 class ib_msg_req_open_orders: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_open_orders(tier2_message_processor *from, tier2_message_processor *to) :
 	  tws_request_message(from, to)
@@ -396,8 +374,6 @@ public:
 /* sends message REQ_ACCOUNT_DATA to IB/TWS */
 class ib_msg_req_account_updates: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_account_updates(tier2_message_processor *from, tier2_message_processor *to, int subscribe, const char acct_code[]) :
 	  tws_request_message(from, to)
@@ -424,8 +400,6 @@ public:
 /* sends message REQ_EXECUTIONS to IB/TWS */
 class ib_msg_req_executions: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_executions(tier2_message_processor *from, tier2_message_processor *to, int reqid, ib_exec_filter *filter) :
 	  tws_request_message(from, to)
@@ -452,8 +426,6 @@ public:
 /* sends message REQ_IDS to IB/TWS */
 class ib_msg_req_ids: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_ids(tier2_message_processor *from, tier2_message_processor *to, int num_ids) :
 	  tws_request_message(from, to)
@@ -480,8 +452,6 @@ public:
 /* sends message REQ_CONTRACT_DATA to IB/TWS */
 class ib_msg_req_contract_details: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_contract_details(tier2_message_processor *from, tier2_message_processor *to, int reqid, ib_contract *contract) :
 	  tws_request_message(from, to)
@@ -508,8 +478,6 @@ public:
 /* sends message REQ_MKT_DEPTH to IB/TWS */
 class ib_msg_req_mkt_depth: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_mkt_depth(tier2_message_processor *from, tier2_message_processor *to, int ticker_id, ib_contract *contract, int num_rows) :
 	  tws_request_message(from, to)
@@ -536,8 +504,6 @@ public:
 /* sends message CANCEL_MKT_DEPTH to IB/TWS */
 class ib_msg_cancel_mkt_depth: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_cancel_mkt_depth(tier2_message_processor *from, tier2_message_processor *to, int ticker_id) :
 	  tws_request_message(from, to)
@@ -564,8 +530,6 @@ public:
 /* sends message REQ_NEWS_BULLETINS to IB/TWS */
 class ib_msg_req_news_bulletins: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_news_bulletins(tier2_message_processor *from, tier2_message_processor *to, int all_msgs) :
 	  tws_request_message(from, to)
@@ -592,8 +556,6 @@ public:
 /* sends message CANCEL_NEWS_BULLETINS to IB/TWS */
 class ib_msg_cancel_news_bulletins: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_cancel_news_bulletins(tier2_message_processor *from, tier2_message_processor *to) :
 	  tws_request_message(from, to)
@@ -620,8 +582,6 @@ public:
 /* sends message SET_SERVER_LOGLEVEL to IB/TWS */
 class ib_msg_set_server_log_level: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_set_server_log_level(tier2_message_processor *from, tier2_message_processor *to, int level) :
 	  tws_request_message(from, to)
@@ -648,8 +608,6 @@ public:
 /* sends message REQ_AUTO_OPEN_ORDERS to IB/TWS */
 class ib_msg_req_auto_open_orders: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_auto_open_orders(tier2_message_processor *from, tier2_message_processor *to, int auto_bind) :
 	  tws_request_message(from, to)
@@ -676,8 +634,6 @@ public:
 /* sends message REQ_ALL_OPEN_ORDERS to IB/TWS */
 class ib_msg_req_all_open_orders: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_all_open_orders(tier2_message_processor *from, tier2_message_processor *to) :
 	  tws_request_message(from, to)
@@ -704,8 +660,6 @@ public:
 /* sends message REQ_MANAGED_ACCTS to IB/TWS */
 class ib_msg_req_managed_accts: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_managed_accts(tier2_message_processor *from, tier2_message_processor *to) :
 	  tws_request_message(from, to)
@@ -732,8 +686,6 @@ public:
 /* sends message REQ_FA to IB/TWS */
 class ib_msg_request_fa: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_request_fa(tier2_message_processor *from, tier2_message_processor *to, int fa_data_type) :
 	  tws_request_message(from, to)
@@ -760,8 +712,6 @@ public:
 /* sends message REPLACE_FA to IB/TWS */
 class ib_msg_replace_fa: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_replace_fa(tier2_message_processor *from, tier2_message_processor *to, int fa_data_type, const char cxml[]) :
 	  tws_request_message(from, to)
@@ -788,8 +738,6 @@ public:
 /* sends message REQ_CURRENT_TIME to IB/TWS */
 class ib_msg_req_current_time: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_current_time(tier2_message_processor *from, tier2_message_processor *to) :
 	  tws_request_message(from, to)
@@ -816,8 +764,6 @@ public:
 /* sends message REQ_FUNDAMENTAL_DATA to IB/TWS */
 class ib_msg_req_fundamental_data: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_fundamental_data(tier2_message_processor *from, tier2_message_processor *to, int reqid, ib_contract *contract, const char report_type[]) :
 	  tws_request_message(from, to)
@@ -844,8 +790,6 @@ public:
 /* sends message CANCEL_FUNDAMENTAL_DATA to IB/TWS */
 class ib_msg_cancel_fundamental_data: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 protected:
 	int reqid;
 
@@ -876,8 +820,6 @@ public:
 /* sends message REQ_CALC_IMPLIED_VOLAT to IB/TWS */
 class ib_msg_calculate_implied_volatility: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_calculate_implied_volatility(tier2_message_processor *from, tier2_message_processor *to, int reqid, ib_contract *contract, double option_price, double under_price) :
 	  tws_request_message(from, to)
@@ -904,8 +846,6 @@ public:
 /* sends message CANCEL_CALC_IMPLIED_VOLAT to IB/TWS */
 class ib_msg_cancel_calculate_implied_volatility: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_cancel_calculate_implied_volatility(tier2_message_processor *from, tier2_message_processor *to, int reqid) :
 	  tws_request_message(from, to)
@@ -932,8 +872,6 @@ public:
 /* sends message REQ_CALC_OPTION_PRICE to IB/TWS */
 class ib_msg_calculate_option_price: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_calculate_option_price(tier2_message_processor *from, tier2_message_processor *to, int reqid, ib_contract *contract, double volatility, double under_price) :
 	  tws_request_message(from, to)
@@ -960,8 +898,6 @@ public:
 /* sends message CANCEL_CALC_OPTION_PRICE to IB/TWS */
 class ib_msg_cancel_calculate_option_price: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_cancel_calculate_option_price(tier2_message_processor *from, tier2_message_processor *to, int reqid) :
 	  tws_request_message(from, to)
@@ -988,8 +924,6 @@ public:
 /* sends message REQ_GLOBAL_CANCEL to IB/TWS */
 class ib_msg_req_global_cancel: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_global_cancel(tier2_message_processor *from, tier2_message_processor *to) :
 	  tws_request_message(from, to)
@@ -1016,8 +950,6 @@ public:
 /* sends message REQ_MARKET_DATA_TYPE to IB/TWS */
 class ib_msg_req_market_data_type: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_req_market_data_type(tier2_message_processor *from, tier2_message_processor *to, tws_market_data_type_t market_data_type) :
 	  tws_request_message(from, to)
@@ -1044,8 +976,6 @@ public:
 /* sends message REQ_REAL_TIME_BARS to IB/TWS */
 class ib_msg_request_realtime_bars: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_request_realtime_bars(tier2_message_processor *from, tier2_message_processor *to, int ticker_id, ib_contract *c, int bar_size, const char what_to_show[], int use_rth) :
 	  tws_request_message(from, to)
@@ -1072,8 +1002,6 @@ public:
 /* sends message CANCEL_REAL_TIME_BARS to IB/TWS */
 class ib_msg_cancel_realtime_bars: public tws_request_message
 {
-	friend class ib_tws_manager;
-
 public:
 	ib_msg_cancel_realtime_bars(tier2_message_processor *from, tier2_message_processor *to, int ticker_id) :
 	    tws_request_message(from, to)
