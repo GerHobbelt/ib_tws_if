472be5266249b2187122c1c041b013867ac2e4ef
Ger Hobbelt <ger@hobbelt.com>
2012-06-06 11:47:36 +0200

    added git pull/push script for submodule updating: no more abort due to multiple copies of the same submod getting out of sync during the update/sync.

---
+++ utils/git_pull_push.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+pushd $(dirname $0)
+cd ..
+
+for f in $( git submodule foreach --quiet $* pwd ) ; do
+	pushd .
+	echo processing PATH/SUBMODULE: $f
+	cd $f
+	git pull --all
+	git push --all
+	popd
+done
+
+popd
+
+
+
