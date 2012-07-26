9a6208e8c36e4f6f206b7ce99a68aaa3432ce5cb
Ger Hobbelt <ger@hobbelt.com>
2012-07-27 01:35:25 +0200

    augment util script to pull/push ALL git repositories in a directory tree, not just the submodules.

--- utils/git_pull_push.sh
+++ utils/git_pull_push.sh
@@ -3,14 +3,32 @@
 pushd $(dirname $0)
 cd ..
 
-for f in $( git submodule foreach --quiet $* pwd ) ; do
-    pushd .
-    echo processing PATH/SUBMODULE: $f
-    cd $f
-    git pull --all
-    git push --all
-    popd
-done
+case x$1 in
+x-f | x/f )
+	shift
+	for f in $( git submodule foreach --quiet $* pwd ) ; do
+		pushd .
+		echo processing PATH/SUBMODULE: $f
+		cd $f
+		#git pull --all
+		#git push --all
+		popd
+	done
+	;;
+	
+* )
+	for f in $( find . -name '.git' ) ; do
+		pushd .
+		f = $( echo $f | sed -e 's#/?\.git$##' )
+		echo processing PATH/SUBMODULE: $f
+		cd $f
+		$*
+		#git pull --all
+		#git push --all
+		popd
+	done
+	;;
+esac
 
 popd
 
